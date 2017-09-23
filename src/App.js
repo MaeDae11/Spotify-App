584d9c299d0f21b496a42275eb911d434dabf96b
Katie <katherinelane11@gmail.com>
2017-09-23 14:20:12 -0400

    adds static search results and reset css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
+import './reset.css';
 import './App.css';
 import './Basic.css';
 
 import SearchBar from './components/SearchBar.js';
+import SearchResults from './components/SearchResults.js';
 
 class App extends Component {
-    
+
   render() {
     return (
       <div className="App">
-        <div className="header">
-            <h1>Ja<span className="highlight">mmm</span>ing</h1>
-        </div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <SearchBar />
-        {/* search box */}
-        {/* search button */}
+            {/* search box */}
+            {/* search button */}
+
+        <div className="App-playlist">
+            <SearchResults />
         {/* results */}
             {/* add to personal list */}
         {/* personal playlist */}
             {/* rename list */}
             {/* minus from list */}
             {/* final save playlist to spotify */}
+        </div>
 
       </div>
     );
