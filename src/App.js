f6cb21e5f527baf375532fedfc96f1cfcb0a49f6
Katie <katherinelane11@gmail.com>
2017-09-23 16:03:03 -0400

    adds spotify search funtion to be passed into searchbar

--- src/App.js
+++ src/App.js
@@ -6,19 +6,38 @@ import './Basic.css';
 import SearchBar from './components/SearchBar.js';
 import SearchResults from './components/SearchResults.js';
 import Playlist from './components/Playlist.js';
+import Spotify from './components/Spotify.js';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            searchResults: [],
+        }
+    }
+
+    spotifySearch = (term) => {
+        Spotify.search(term)
+        .then(searchResults => {
+            if(searchResults.length > 0){
+                searchResults: [];
+            }
+            this.setState({
+                searchResults: searchResults
+            })
+        })
+    }
 
   render() {
     return (
       <div className="App">
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
-        <SearchBar />
+        <SearchBar spotifySearch={this.spotifySearch}/>
             {/* search box */}
             {/* search button */}
 
         <div className="App-playlist">
-            <SearchResults />
+            <SearchResults searchResults={this.state.searchResults}/>
         {/* results */}
             {/* add to personal list */}
             <Playlist />
